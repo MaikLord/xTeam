b9a4917f62d8c1d7e20b4ef7978853f16d0e1dae
maiklord <lucas32150@hotmail.com>
2021-10-21 18:51:35 -0300

    New Update page x Team

---
+++ script.js
@@ -0,0 +1,13 @@
+var phrase = "estágio na Raccoon";
+var letter = "a";
+var quanty = 0;
+
+for (var i = 0; i < phrase.length; i++) {
+    if (phrase[i] == letter) {
+        quanty++
+    }
+}
+
+console.log(`A Letra : ${letra.toUpperCase(letra)} , Aparece : ${quantidade} Vezes`);
+
+
